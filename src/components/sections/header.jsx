434ab1ee4a8e131b9e0ca5781c99b7d7028867e9
efuaboy11 <iseghohimhene@gmail.com>
2025-11-26 23:15:47 +0100

    changes

--- src/components/sections/header.jsx
+++ src/components/sections/header.jsx
@@ -44,10 +44,10 @@ const Header = () => {
                                 <div className="navbar-header">
                                     <div className="mobile-logo">
                                         <Link href="/">
-                                            {/* <Image width={75} height={25} sizes='100vw' src={"/images/logo.png"} alt="Logo" title="Logo" /> */}
+                                            <Image width={75} height={25} sizes='100vw' src={"/images/logo.png"} alt="Logo" title="Logo" />
                                         </Link>
                                     </div>
-
+                                    
                                     {/* <!-- Toggle Button --> */}
                                     <button type="button" className="navbar-toggle" data-bs-toggle="collapse" data-bs-target=".navbar-collapse">
                                         <span className="icon-bar"></span>
